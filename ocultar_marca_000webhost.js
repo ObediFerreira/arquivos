4ae6ba5eda9e612697a2d774812f5ac83c18b2f9
ObediFerreira <48829836+ObediFerreira@users.noreply.github.com>
2024-08-17 00:52:24 -0300

    Delete ocultar_marca_000webhost.js

--- ocultar_marca_000webhost.js
+++
@@ -1,9 +0,0 @@
-setInterval(() => {
-    var content = "img[alt='www.000webhost.com']"
-    var imgElement = document.querySelector(content);
-    if (imgElement) {
-        imgElement.style.display = "none";
-        imgElement.remove();
-        clearInterval();
-    }
-}, 100);
